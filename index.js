8cc67231e20afc3907210390f165806299c6132c
DrFriendless <Borod1n>
2017-10-15 11:23:06 +1100

    try to add a second lambda

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 'use strict';
 var fs = require('fs');
 
- exports.get = function(event, context) {
-   var contents = fs.readFileSync("public/index.html");
-   context.succeed({
-     statusCode: 200,
-     body: contents.toString(),
-     headers: {'Content-Type': 'text/html'}
-   });
- };
+exports.get = function(event, context) {
+    var contents = fs.readFileSync("public/index.html");
+    context.succeed({
+        statusCode: 200,
+        body: contents.toString(),
+        headers: {'Content-Type': 'text/html'}
+    });
+};
+exports.john = function(event, context) {
+    context.succeed({
+        statusCode: 200,
+        body: "Hello John",
+        headers: {'Content-Type': 'text/plain'}
+    });
+};
