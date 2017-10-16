874444e926de2773e3c8e1b21406d23b8f118638
DrFriendless <friendless.farrell@gmail.com>
2017-10-16 21:54:55 +1100

    trying to stick SNS in there as well

--- users.ts
+++ users.ts
@@ -4,36 +4,35 @@ const request = require('request');
 
 // Lambda to get the list of users from an SQS queue and write it to Mongo DB.
 export const writeToDB: Handler = (event, context, callback: Callback) => {
-    const sqs = new SQS({region : process.env.AWS_REGION});
-    const params: SQS.Types.ReceiveMessageRequest = {
-        QueueUrl: process.env.QUEUE_URL,
-        AttributeNames: ["All"],
-    };
-    sqs.receiveMessage(params, (err, data: SQS.Types.ReceiveMessageResult) => {
-        if (err) {
-            console.log('error:',"Fail Receive Message" + err);
-            return callback(new Error("SQS receive failed"));
-        } else {
-            console.log(data.Messages);
-            if (data.Messages) {
-                data.Messages.forEach(message => {
-                    if (message.ReceiptHandle) {
-                        const params: SQS.Types.DeleteMessageRequest = {
-                            QueueUrl: process.env.QUEUE_URL,
-                            ReceiptHandle: message.ReceiptHandle
-                        };
-                        sqs.deleteMessage(params);
-                    }
-                });
-            }
-        }
-    });
+    // const sqs = new SQS({region : process.env.AWS_REGION});
+    // const params: SQS.Types.ReceiveMessageRequest = {
+    //     QueueUrl: process.env.QUEUE_URL,
+    //     AttributeNames: ["All"],
+    // };
+    // sqs.receiveMessage(params, (err, data: SQS.Types.ReceiveMessageResult) => {
+    //     if (err) {
+    //         console.log('error:',"Fail Receive Message" + err);
+    //         return callback(new Error("SQS receive failed"));
+    //     } else {
+    //         console.log(data.Messages);
+    //         if (data.Messages) {
+    //             data.Messages.forEach(message => {
+    //                 if (message.ReceiptHandle) {
+    //                     const params: SQS.Types.DeleteMessageRequest = {
+    //                         QueueUrl: process.env.QUEUE_URL,
+    //                         ReceiptHandle: message.ReceiptHandle
+    //                     };
+    //                     sqs.deleteMessage(params);
+    //                 }
+    //             });
+    //         }
+    //     }
+    // });
+    console.log(event);
 };
 
 // Lambda to get the list of users from pastebin and stick it on a queue to be processed.
 export const readFromPastebin: Handler = (event, context, callback: Callback) => {
-    console.log("region = " + process.env.AWS_REGION);
-    console.log("queue = " + process.env.QUEUE_URL);
     const sqs = new SQS({region : process.env.AWS_REGION});
     request("https://pastebin.com/raw/BvvdxzcH", (error, response, body) => {
         if (error) {
