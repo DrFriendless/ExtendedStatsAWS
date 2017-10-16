f5c87a22f96558124a9a38666ff4067caea224da
DrFriendless <Borod1n>
2017-10-16 22:40:37 +1100

    trying to wire in SNS

--- users.ts
+++ users.ts
@@ -1,5 +1,5 @@
 import {Callback, Handler} from 'aws-lambda';
-import {SQS} from 'aws-sdk';
+import {SQS, SNS} from 'aws-sdk';
 const request = require('request');
 
 // Lambda to get the list of users from an SQS queue and write it to Mongo DB.
@@ -33,21 +33,34 @@ export const writeToDB: Handler = (event, context, callback: Callback) => {
 
 // Lambda to get the list of users from pastebin and stick it on a queue to be processed.
 export const readFromPastebin: Handler = (event, context, callback: Callback) => {
-    const sqs = new SQS({region : process.env.AWS_REGION});
+    // const sqs = new SQS({region : process.env.AWS_REGION});
+    const snsEndpoint = process.env.SNS_ENDPOINT;
+    const sns = new SNS();
     request("https://pastebin.com/raw/BvvdxzcH", (error, response, body) => {
         if (error) {
             console.log("Retrieve-user-list failed: " + error);
             return callback(new Error(error));
         }
-        const params: SQS.Types.SendMessageRequest = {
-            'MessageBody': body,
-            'QueueUrl': process.env.QUEUE_URL
-        };
-        sqs.sendMessage(params, (err, data: SQS.Types.SendMessageResult) => {
+        sns.publish({
+            Message: body,
+            TargetArn: snsEndpoint
+        }, function(err, data) {
             if (err) {
-                console.log('error:',"Fail Send Message" + err);
-                return callback(new Error("SQS send failed"));
+                console.log(err.stack);
+                return;
             }
+            console.log('push sent');
+            console.log(data);
         });
+        // const params: SQS.Types.SendMessageRequest = {
+        //     'MessageBody': body,
+        //     'QueueUrl': process.env.QUEUE_URL
+        // };
+        // sqs.sendMessage(params, (err, data: SQS.Types.SendMessageResult) => {
+        //     if (err) {
+        //         console.log('error:',"Fail Send Message" + err);
+        //         return callback(new Error("SQS send failed"));
+        //     }
+        // });
     });
 };
