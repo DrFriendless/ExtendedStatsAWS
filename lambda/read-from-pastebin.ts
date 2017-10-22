45e8fa682822cb8cdad74c95ce645e799166f67a
DrFriendless <friendless.farrell@gmail.com>
2017-10-22 14:01:06 +1100

    try to debug this

--- lambda/read-from-pastebin.ts
+++ lambda/read-from-pastebin.ts
@@ -1,24 +1,8 @@
-import {Callback, Handler} from 'aws-lambda';
 import {SNS} from 'aws-sdk';
 import {IncomingMessage} from "https";
-import {getConfig} from "./config";
-import {ensureUser} from "./mongo";
-const https = require('https');
+import {Callback, Handler} from "aws-lambda";
 
-// Lambda to get the list of users from an SQS queue and write it to Mongo DB.
-export const writeToDB: Handler = (event, context, callback: Callback) => {
-    // const SnsMessageId = event.Records[0].Sns.MessageId;
-    // const SnsPublishTime = event.Records[0].Sns.Timestamp;
-    // const SnsTopicArn = event.Records[0].Sns.TopicArn;
-    const body = event.Records[0].Sns.Message;
-    console.log(body);
-    const usernames = body.split(/\r?\n/);
-    getConfig(config => {
-        usernames.forEach((username: string) => {
-            ensureUser(config, username);
-        });
-    });
-};
+const https = require('https');
 
 // Lambda to get the list of users from pastebin and stick it on a queue to be processed.
 export const readFromPastebin: Handler = (event, context, callback: Callback) => {
@@ -45,5 +29,4 @@ export const readFromPastebin: Handler = (event, context, callback: Callback) =>
             });
         });
     });
-
 };
